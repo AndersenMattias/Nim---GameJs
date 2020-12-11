aa9f871c3c03860f0d7129d6208f193e1dd45c4b
MattiasSimonsson <Mattias.simonsson1@gmail.com>
2020-12-11 12:21:32 +0100

    added function for removing amount sticks

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-let numOfSticks = 21;
+let stickPile = 21;
 let playerOne;
 let playerTwo;
+
+let sticksDiv = document.querySelector('.sticks-div');
+sticksDiv.innerHTML = stickPile;
+
+const takeSticks = (number) => {
+  let removeSticks = (stickPile -= number);
+  sticksDiv.innerHTML = removeSticks;
+};
